84a3eaa67cd4528380304c8ba85f2ccf5c5add3e
Hariom Yadav <hariom.yadav@astridinfotech.com>
2023-09-12 19:00:59 +0530

    rc-react

--- datatable/src/App.js
+++ datatable/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import DataTable from './components/datatables/DataTable';
 import DataTables from './components/datatablecomponets/DataTables';
 import TableWrapper from './components/datatables/DataTable';
+import YearRangeSlider from './components/reactSlider/ReactSlider';
 
 function App() {
   return (
     <div className="App">
     {/* <DataTables /> */}
     <TableWrapper />
+    <YearRangeSlider />
     </div>
   );
 }
