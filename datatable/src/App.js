0181a429b8daf047abe60b7ac2a1efab5bb73cef
Hariom Yadav <hariom.yadav@astridinfotech.com>
2023-09-12 14:14:25 +0530

    Slider

---
+++ datatable/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import DataTable from './components/datatables/DataTable';
+import DataTables from './components/datatablecomponets/DataTables';
+import TableWrapper from './components/datatables/DataTable';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <DataTables /> */}
+    <TableWrapper />
+    </div>
+  );
+}
+
+export default App;
