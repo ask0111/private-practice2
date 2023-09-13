d874f23cb2ac5f6fd2315027db551de0086b3993
Hariom Yadav <hariom.yadav@astridinfotech.com>
2023-09-13 15:33:57 +0530

    Multi step form

--- datatable/src/App.js
+++ datatable/src/App.js
@@ -4,13 +4,22 @@ import DataTable from './components/datatables/DataTable';
 import DataTables from './components/datatablecomponets/DataTables';
 import TableWrapper from './components/datatables/DataTable';
 import YearRangeSlider from './components/reactSlider/ReactSlider';
+import MultiStepForm from './components/mutliStepForm/MultiStepForm';
 
 function App() {
   return (
     <div className="App">
     {/* <DataTables /> */}
-    <TableWrapper />
-    <YearRangeSlider />
+    {/* <TableWrapper />
+    <YearRangeSlider /> */}
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+        <MultiStepForm />
+        </div>
+      </div>
+    </div>
+    
     </div>
   );
 }
